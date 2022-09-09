6b918d86ace26c979a50f59234a1a753230d1a22
smpandharkar <100663291+smpandharkar@users.noreply.github.com>
2022-09-09 15:19:40 -0400

    Delete testNGBatchFile.bat

--- CucumberJava/testNGBatchFile.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\swati\eclipse-workspace\CucumberJava
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
